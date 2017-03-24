760585150e7b31893b664d6638ff1c391e515ce8
Zhou Moliang <endeavourwilliam.zhou@gmail.com>
2017-03-24 16:41:02 -0400

    2017.03.24 detail info, url debug

--- Connect_mysql.py
+++ Connect_mysql.py
@@ -14,23 +14,53 @@ class mysql:
         self.db = sql.connect(host="wowstats.cctqbu5psiq5.us-east-2.rds.amazonaws.com", port=3306, user="moliangzhou",
                               password="33906413", database="wowstats")
 
-    def write_db(self, data_list):
+    def get_IDlist(self):
         cursor = self.db.cursor()
-        insert_sql = """INSERT INTO `wows_account` (`account ID`, `nickname`) VALUES %s"""
-        for record in data_list:
-            self.write_single(insert_sql=insert_sql, record=record, cursor=cursor)
-        print("********************Database write finished********************")
-
-    def write_single(self, insert_sql, record, cursor):
+        getid_sql = """SELECT `accountID` FROM wowstats.`wows_stats`"""
         try:
             # execute sql in database
-            cursor.execute(query=insert_sql, args=[record])
-            self.db.commit()
-            print("%s written." % (record,))
+            cursor.execute(query=getid_sql)
+            return cursor.fetchall()
         except:
             # roll back if error
             self.db.rollback()
-            print("%s write failed!" % (record,))
+            print("Fetch failed!!!")
+
+    def write_ID(self, data_list):
+        cursor = self.db.cursor()
+        insert_sql = """INSERT INTO `wowstats`.`wows_stats` (`accountID`, `nickname`) VALUES %s"""
+        for record in data_list:
+            try:
+                # execute sql in database
+                cursor.execute(query=insert_sql, args=[record])
+                self.db.commit()
+                print("%s written." % (record,))
+            except:
+                # roll back if error
+                self.db.rollback()
+                print("%s write failed!" % (record,))
+        print("********************Database write finished********************")
+
+    def write_detail(self, data_list):
+        cursor = self.db.cursor()
+        update_sql = """
+        UPDATE `wowstats`.`wows_stats`
+        SET `nickname` = %s,`total` = %s, `win`= %s, `defeat`= %s, `draw`= %s
+        WHERE `accountID`=%s
+        """
+        for record in data_list:
+
+            try:
+                # execute sql in database
+                cursor.execute(query=update_sql,
+                               args=[record[1], record[2], record[3], record[4], record[5], record[0]])
+                self.db.commit()
+                print("%s written." % (record,))
+            except:
+                # roll back if error
+                self.db.rollback()
+                print("%s write failed!" % (record,))
+        print("********************Database write finished********************")
 
     def close_db(self):
         # disconnect
@@ -40,7 +70,7 @@ class mysql:
 if __name__ == '__main__':
     try:
         db = mysql()
-        # db.write_db(data="")
+        db.write_detail(data_list=[('1018170999', 'Luizclv', '0', '0', '0', '0')])
         db.close_db()
     except ConnectionError:
         print("Database connection failed!")
--- WOWS_API.py
+++ WOWS_API.py
@@ -12,7 +12,7 @@ from Connect_mysql import mysql
 # elseif ($id < 3000000000) return 'ASIA';
 # elseif ($id >= 3000000000) return 'KR';
 
-
+size_per_write = 1000
 
 def check_ip():
     external_ip = ipgetter.myip()
@@ -21,62 +21,134 @@ def check_ip():
     print("Local ip:%s " % local_ip)
 
 
-def request_player():
-    # Request API url
-    # player_url = 'https://api.worldofwarships.com/wows/account/list/'
-    account_url = 'https://api.worldofwarships.com/wows/account/info/'
-    # Request params
-    application_id = 'bc7a1942582313fd553a85240bd491c8'
-    account_ID = 1025477300
-    result_list = []
-    size_per_write = 100
-
-    while account_ID < 2000000000:
-        idlist, account_ID = create_idlist(account_ID)
-        parameter = parse.urlencode({'application_id': application_id, 'account_id': idlist}, True)
-
-        url = account_url + '?' + parameter
-
-        try:
-            result = request.urlopen(url).read().decode("utf-8")
-            # print(result)
-            data = json.loads(result)
-            if data["status"] == "ok":
-                for acc_id in data["data"]:
-                    if data["data"][acc_id] is not None:
-                        nickname = data["data"][acc_id]["nickname"]
-                        record = (str(acc_id), str(nickname))
-                        result_list.append(record)
-            else:
-                print(data["error"])  # print error message
-        except error.URLError:  # API url request failed
-            print("API request failed!")
-        if len(result_list) >= size_per_write:  # write when data has 100 records
-            write_database(result_list)
-            result_list = []
-
-    return "Request finished!"
-
-
-def write_database(data_list):
+def get_idlistfromsql():
     try:
         db = mysql()
-        db.write_db(data_list)
+        idlist = db.get_IDlist()
         db.close_db()
+        return idlist
     except ConnectionError:
-        print("Database connection failed!")
+        print("Get ID list connection failed!")
 
 
 def create_idlist(account_ID):
     id = []
     Length = 100
-    account_ID /= 100
+    # account_ID /= 100
     for i in range(Length):
-        id.append((int)(100 * (account_ID + i) + 99))
-    return id, (account_ID + Length) * 100
+        id.append(int(account_ID + i))
+    return id, account_ID + Length
+
+
+def convertlisttopara(list):
+    s = ""
+    for i in list:
+        if (s != ""):
+            s += ","
+        s += str(i)
+    return s
+
+
+def request_statsbyID(account_url, application_id):
+    result_list = []
+    idlist = get_idlistfromsql()
+    sublist = []
+    for id in idlist:
+        sublist.append(id[0])
+        if len(sublist) == 100:
+            sublist = convertlisttopara(sublist)
+            parameter = parse.urlencode({'application_id': application_id, 'account_id': sublist})
+            url = account_url + '?' + parameter
+            try:
+                result = request.urlopen(url).read().decode("utf-8")
+                # print(result)
+                data = json.loads(result)
+                # result_list = record_ID(data, result_list)
+                result_list = record_detail(data, result_list)
+                sublist = []
+            except error.URLError:  # API url request failed
+                print("API request failed!")
+
+
+def request_allID(account_url, application_id):
+    account_ID = 1000000000
+    result_list = []
+
+    while account_ID < 2000000000:
+        idlist, account_ID = create_idlist(account_ID)
+        idlist = convertlisttopara(idlist)
+        parameter = parse.urlencode({'application_id': application_id, 'account_id': idlist})
+        url = account_url + '?' + parameter
+        try:
+            result = request.urlopen(url).read().decode("utf-8")
+            # print(result)
+            data = json.loads(result)
+            result_list = record_ID(data, result_list)
+        except error.URLError:  # API url request failed
+            print("API request failed!")
+
+
+def record_ID(data, result_list):
+
+    if data["status"] == "ok":
+        for acc_id in data["data"]:
+            if data["data"][acc_id] is not None:
+                nickname = data["data"][acc_id]["nickname"]
+                record = (str(acc_id), str(nickname))
+                result_list.append(record)
+    else:
+        print(data["error"])  # print error message
+    if len(result_list) >= size_per_write:  # write when data has 100 records
+        try:
+            db = mysql()
+            db.write_ID(result_list)
+            db.close_db()
+        except ConnectionError:
+            print("Database connection failed!")
+        result_list = []
+    return result_list
+
+
+def record_detail(data, result_list):
+    if data["status"] == "ok":
+        for acc_id in data["data"]:
+            case = data["data"][acc_id]
+            if case is not None and not case["hidden_profile"]:
+                nickname = case["nickname"]
+                pvp = case["statistics"]["pvp"]
+                total = pvp["battles"]
+                win = pvp["wins"]
+                defeat = pvp["losses"]
+                draw = pvp["draws"]
+                record = (str(acc_id), str(nickname), str(total), str(win), str(defeat), str(draw))
+                result_list.append(record)
+            else:
+                print("User data private or null")
+    else:
+        print(data["error"])  # print error message
+    if len(result_list) >= size_per_write:  # write when data has 100 records
+        try:
+            db = mysql()
+            db.write_detail(result_list)
+            db.close_db()
+        except ConnectionError:
+            print("Database connection failed!")
+        result_list = []
+    return result_list
+
+
+def request_API():
+    # Request API url
+    # player_url = 'https://api.worldofwarships.com/wows/account/list/'
+    account_url = 'https://api.worldofwarships.com/wows/account/info/'
+    # Request params
+    application_id = 'bc7a1942582313fd553a85240bd491c8'
+    # request_allID(account_url, application_id)
+    request_statsbyID(account_url, application_id)
+    return "Request finished!"
 
 
 if __name__ == '__main__':
     check_ip()
-    result = request_player()
+    result = request_API()
     print(result)
