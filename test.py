42786292863ce0ed15345020a21d6986b8b7c59b
Zhou Moliang <endeavourwilliam.zhou@gmail.com>
2017-06-02 16:27:27 -0400

    2017.06.02 Test functions

--- test.py
+++ test.py
@@ -1,15 +1,42 @@
 import WOWS.WOWS_API as wows
 import util.read_config as config
-import coverage
+import pymysql as sql
+from WOWS.WOWS_RDS import wows_database
 
 
 def test_WOWS_API():
-    wows.check_ip()
-    wows.check_date()
+    try:
+        wows.check_ip()
+        wows.check_date()
+        idlist = wows.create_idlist(account_ID=1000000000)
+        wows.convertlisttopara(idlist)
+        wows.request_main(days=1)
+    except:
+        print("WOWS API test failed!")
+
+
+def test_WOWS_RDS():
+    try:
+        db = wows_database()
+        db.write_detail(data_list=[('1018170999', 'Luizclv', '0', '0', '0', '0')])
+        id_list = db.get_IDlist()
+        print(id_list)
+        db.write_ID(data_list=['1000000000', 'xxxxxxx'])
+        db.close_db()
+    except sql.MySQLError:
+        print("WOWS RDS test failed!")
+
+
+def test_config():
+    try:
+        cg = config.config()
+        json_data = cg.read_config(config_file="sample_config.json")
+        print(json_data)
+    except:
+        print("read_config test failed!")
 
 
 if __name__ == "__main__":
     test_WOWS_API()
-    cg = config.config()
-    json_data = cg.read_config(config_file="sample_config.json")
-    print(json_data)
+    test_WOWS_RDS()
+    test_config()
