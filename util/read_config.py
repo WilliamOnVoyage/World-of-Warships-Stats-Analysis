8119a8ba3972a724f1942c025290210cca1bd599
Zhou Moliang <endeavourwilliam.zhou@gmail.com>
2017-06-02 12:11:00 -0400

    2017.06.02 coverage version, sample config

--- util/read_config.py
+++ util/read_config.py
@@ -6,15 +6,15 @@ class config(object):
     def __init__(self):
         self.path = os.path.split(os.path.realpath(__file__))[0]
 
-    def read_config(self):
+    def read_config(self, config_file="config.json"):
         path = os.path.dirname(self.path)
-        file = os.path.join(path, "config.json")
+        file = os.path.join(path, config_file)
         json_data = open(file).read()
         return json_data
 
 
 if __name__ == '__main__':
     cg = config()
-    json_data = cg.read_config()
+    json_data = cg.read_config(config_file="sample_config.json")
     json_str = json.loads(json_data)
     print(json_str['mysql']['usr'])
