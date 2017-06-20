d4df3268de395b67015e5bd2126afac955433489
Zhou Moliang <endeavourwilliam.zhou@gmail.com>
2017-06-20 13:49:11 -0400

    2017.06.19 Add model, pandas panel dataframe
    To finish:
     training procedure

--- main/test.py
+++ main/test.py
@@ -1,22 +1,18 @@
 import datetime
-import os
-import sys
 
 import pymysql as sql
 
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
-
+import api_database.wows_api as wows_api
+import api_database.wows_db as wows_db
 from util import read_config as config
 from util import utility as ut
-from api_database.wows_api import wows_api_req
-from api_database.wows_db import wows_database
+
 
 def test_wows_api():
     try:
         ut.check_ip()
         ut.check_date()
-        wows = wows_api_req()
+        wows = wows_api.wows_api_req()
         idlist = wows.create_idlist(account_ID=1000000000)
         wows.convertlisttopara(idlist)
         wows.update_winRate(date=datetime.datetime.now().date())
@@ -27,7 +23,7 @@ def test_wows_api():
 
 def test_wows_rds():
     try:
-        db = wows_database()
+        db = wows_db.wows_database()
         db.write_detail(data_list=[('2017-01-01', '1000000000', 'xxxxxxx', '1', '0', '0', '0', '0')])
         id_list = db.get_IDlist()
         print(id_list)
