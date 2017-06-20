456a9f229fd72ed21bbc48192ee141cfdd4628a8
Zhou Moliang <endeavourwilliam.zhou@gmail.com>
2017-06-19 23:09:00 -0400

    2017.06.19 Add model, pandas panel dataframe
    To finish:
     training procedure

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import datetime
 
 import pymysql as sql
 
-from api_database import wows_api as wows
+from api_database.wows_api import wows_api
 from api_database.wows_db import wows_database
 from util import read_config as config
 from util import utility as ut
@@ -12,10 +12,10 @@ def test_wows_api():
     try:
         ut.check_ip()
         ut.check_date()
-        wows_api = wows.wows_api()
-        idlist = wows_api.create_idlist(account_ID=1000000000)
-        wows_api.convertlisttopara(idlist)
-        wows_api.update_winRate(date=datetime.datetime.now().date())
+        wows = wows_api()
+        idlist = wows.create_idlist(account_ID=1000000000)
+        wows.convertlisttopara(idlist)
+        wows.update_winRate(date=datetime.datetime.now().date())
         wows.main(days=1)
     except:
         print("api_database API test failed!")
