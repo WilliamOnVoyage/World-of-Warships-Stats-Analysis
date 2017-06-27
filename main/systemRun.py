fc1aefdd551a1ff7919cb8acce739f840a92b5c4
Zhou Moliang <endeavourwilliam.zhou@gmail.com>
2017-06-27 11:06:46 -0400

    2017.06.27 Reformat

--- main/systemRun.py
+++ main/systemRun.py
@@ -12,6 +12,8 @@ from util.ansi_code import ANSI_escode as ansi
 
 def database_update(date):
     # Request params from config file
+    start = datetime.datetime.now()
+    last_date = start.date()
     cg = read_config.config()
     config_data = json.loads(cg.read_config())
     application_id = config_data['wows_api']['application_id']
@@ -21,9 +23,16 @@ def database_update(date):
     wows_api = wows.wows_api_req()
     wows_api.request_statsbyID(account_url, application_id, date, overwrite=True)
     wows_api.update_winRate(date)
+    end = datetime.datetime.now()
+    db_time = end - start
+    print("\n%s%s%s data update finished, time usage: %s%s%s\n" % (
+        ansi.BLUE, last_date.strftime("%y-%m-%d"), ansi.ENDC, ansi.DARKGREEN, db_time, ansi.ENDC))
+    return db_time
 
 
 def model_update(date):
+    start = datetime.datetime.now()
+    last_date = start.date()
     timewindow = 8
     # retrieve 8 days data, last day is the y_trn and previous 7 days are the x_trn
     # Data in pandas.Panel format
@@ -32,6 +41,11 @@ def model_update(date):
     # initialize model
     model = winR_model(x_trn=x_trn, y_trn=y_trn, x_val=x_val, y_val=y_val, time_step=timewindow - 1)
     model.train_case(contd=False)
+    end = datetime.datetime.now()
+    model_time = end - start
+    print("\n%s%s%s model update finished, time usage: %s%s%s\n" % (
+        ansi.BLUE, last_date.strftime("%y-%m-%d"), ansi.ENDC, ansi.DARKGREEN, model_time, ansi.ENDC))
+    return model_time
 
 
 def systemRun(days=7):
@@ -40,21 +54,11 @@ def systemRun(days=7):
     while day_count != 0:
         start = datetime.datetime.now()
         if start.date() != last_date:
+            db_time = datetime.timedelta(0)
+            model_time = datetime.timedelta(0)
             last_date = start.date()
-            database_update(date=last_date)
-            end = datetime.datetime.now()
-
-            db_time = end - start
-            print("\n%s%s%s data update finished, time usage: %s%s%s\n" % (
-                ansi.BLUE, last_date.strftime("%y-%m-%d"), ansi.ENDC, ansi.DARKGREEN, db_time, ansi.ENDC))
-
-            start = datetime.datetime.now()
-            model_update(date=last_date)
-            end = datetime.datetime.now()
-            model_time = end - start
-            print("\n%s%s%s model update finished, time usage: %s%s%s\n" % (
-                ansi.BLUE, last_date.strftime("%y-%m-%d"), ansi.ENDC, ansi.DARKGREEN, model_time, ansi.ENDC))
-
+            db_time = database_update(date=last_date)
+            # model_time = model_update(date=last_date)
             print("\n%s%s%s function finished, total time usage: %s%s%s\n" % (
                 ansi.BLUE, last_date.strftime("%y-%m-%d"), ansi.ENDC, ansi.DARKGREEN, db_time + model_time,
                 ansi.ENDC))
